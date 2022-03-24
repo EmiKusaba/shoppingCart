c52d3c090bc95bfd70cafe027f03723bc6fe1aac
EmiKusaba <eeemi615@gmail.com>
2022-03-24 14:39:19 -0500

    added structure

--- practice/main2.js
+++ practice/main2.js
@@ -14,19 +14,38 @@ class ShoppingCart {
   }
 
   add(item) {
-    for(let i = 0; i < this.items.length; i++) {
+    for (let i = 0; i < this.items.length; i++) {
       let exsistingItemAndQuantity = this.items[i]
-      if(exsistingItemAndQuantity.item.name === item.name ) {
+      if (exsistingItemAndQuantity.item.name === item.name) {
         existingItemAndQuantity.quantity += 1
         return
-
       }
-      this.items.push(
-        {
-          "item": item,
-          "quantity": 1
-        }
-      )
     }
+    this.items.push(
+      {
+        "item": item,
+        "quantity": 1
+      }
+    )
+  }
+
+  getTotalCost() {
+
   }
 }
+
+const addItemToCart = () => {
+
+}
+
+const refreshCart = () => {
+
+}
+
+const availableItems = [
+
+]
+
+let myCart = new ShoppingCart()
+
+//List DOM
