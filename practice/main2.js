4bc87192149f6a881b8ddeda11b478d3ba2e7dc4
EmiKusaba <eeemi615@gmail.com>
2022-03-28 15:07:50 -0500

    added listDOM

--- practice/main2.js
+++ practice/main2.js
@@ -35,31 +35,53 @@ class ShoppingCart {
 }
 
 const addItemToCart = (cart, item, quantity) => {
-cart.add(item, quantity)
-refreshCart(cart)
+  cart.add(item, quantity)
+  refreshCart(cart)
 }
 
 const refreshCart = (cart) => {
-let cartDOM = document.getElementsById("cart")
-cartDOM.innerHTML = ""
-for( i = 0; i < cart.items.length; i++) {
-  const item = cart.items[i]
+  let cartDOM = document.getElementsById("cart")
+  cartDOM.innerHTML = ""
+  for (i = 0; i < cart.items.length; i++) {
+    const item = cart.items[i]
 
-  let quantityDOM = document.createElement("div")
-  quantityDOM.style = ".grid-column: 1 / 2;"
-  quantityDOM.innerHTML = `${item.quantity} x`
-  cartDOM.appendChild(quantityDOM)
-}
+    let quantityDOM = document.createElement("div")
+    quantityDOM.style = ".grid-column: 1 / 2;"
+    quantityDOM.innerHTML = `${item.quantity} x`
+    cartDOM.appendChild(quantityDOM)
+  }
 }
 
 const availableItems = [
-new Item("Banana", 1.00),
-new Item("Strawberry Pie", 2.00),
-new Item("Chocochip Cookie", 3.00),
-new Item("Water", 1.00),
-new Item("Lemonade", 2.00)
+  new Item("Banana", 1.00),
+  new Item("Strawberry Pie", 2.00),
+  new Item("Chocochip Cookie", 3.00),
+  new Item("Water", 1.00),
+  new Item("Lemonade", 2.00)
 ]
 
 let myCart = new ShoppingCart()
 
 //List DOM
+let listDOM = document.getElementById("list")
+for (let i = 0; i < availableItems.length; i++) {
+  const item = availableItems[i]
+
+  let nameDOM = document.createElement("div")
+  nameDOM.style = "grid-column: 1 / 2;"
+  nameDOM.innerHTML = `${item.name}`
+  listDOM.appendChild(nameDOM)
+
+  let priceDOM = document.createElement("div")
+  priceDOM.style = "grid-column: 2 / 3;"
+  priceDOM.innerHTML = `$${item.price}`
+  listDOM.appendChild(priceDOM)
+
+  let addToCartDOM = document.createElement("div")
+  addToCartDOM.style = "grid-column: 3 / 4"
+  addToCartDOM.className = "material-icons"
+  addToCartDOM.innerHTML = "add_shopping_cart"
+  addToCartDOM.addEventListener("click", () => addToCartDOM(myCart, item))
+  listDOM.appendChild(addToCartDOM)
+
+}
