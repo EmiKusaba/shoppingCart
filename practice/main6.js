2df7d61d7cc3d7761763931d36c173cc968e2e66
EmiKusaba <eeemi615@gmail.com>
2022-04-19 14:55:00 -0500

    added getTotalCost func

--- practice/main6.js
+++ practice/main6.js
@@ -1,7 +1,7 @@
 //Item class
 
 class Item {
-  constructor(name, price){
+  constructor(name, price) {
     this.name = name
     this.price = price
   }
@@ -10,15 +10,42 @@ class Item {
 //ItemAndQuantity class
 
 class ItemAndQuantity {
-  constructor(item, quantity){
-  this.item = item
-  this.quantity= quantity
+  constructor(item, quantity) {
+    this.item = item
+    this.quantity = quantity
   }
 }
 
 
 //ShoppingCart class
 
+class ShoppingCart {
+  constructor() {
+    this.itemAndQuantities = []
+  }
+  add(item) {
+    for (let i = 0; i < this.itemAndQuantities.length; i++) {
+      let exsistingItemAndQuantity = this.itemAndQuantities[i]
+      if (exsistingItemAndQuantity.item.name === item.name) {
+        exsistingItemAndQuantity.quantity += 1
+        return
+      }
+    }
+    this.itemAndQuantities.push(
+      new ItemAndQuantity(item, 1)
+    )
+  }
 
+  getTotalCost() {
+    let totalCost = 0
+
+    for (let i = 0; i < this.itemAndQuantities.length; i++) {
+      let itemAndQuantity = this.itemAndQuantities[i]
+      totalCost += itemAndQuantity.item.price * itemAndQuantity.quantity
+    }
+
+    return totalCost
+  }
+}
 
 //ListDOM
