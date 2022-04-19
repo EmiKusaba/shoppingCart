806d600b10ab30a8c2d12fb8d6a6178d906a6f22
EmiKusaba <eeemi615@gmail.com>
2022-04-19 14:30:26 -0500

    added class Item and ItemAndQUantity

---
+++ practice/main6.js
@@ -0,0 +1,24 @@
+//Item class
+
+class Item {
+  constructor(name, price){
+    this.name = name
+    this.price = price
+  }
+}
+
+//ItemAndQuantity class
+
+class ItemAndQuantity {
+  constructor(item, quantity){
+  this.item = item
+  this.quantity= quantity
+  }
+}
+
+
+//ShoppingCart class
+
+
+
+//ListDOM
