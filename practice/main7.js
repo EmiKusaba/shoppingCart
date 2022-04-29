1b2fe2d0aafbfc0e82c63138a172241af7694085
EmiKusaba <eeemi615@gmail.com>
2022-04-29 14:56:31 -0500

    added ItemAndQuantity and ShoppingCArt class

--- practice/main7.js
+++ practice/main7.js
@@ -6,10 +6,20 @@ class Item {
   }
 }
 
-
-
 //ItemAndQuantity
+class ItemAndQuantity {
+  constructor(item,quantity) {
+   this.item = item
+   this.quantity = quantity
+  }
+}
+//ShoppingCart
 
+class ShoppingCart {
+ constructor() {
+   this.ItemAndQuantities = []
+ }
+}
 
 
 //avairableItem
