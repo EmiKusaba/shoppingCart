2692f6719a093b7089f23e7233d2f890af6a81d9
EmiKusaba <eeemi615@gmail.com>
2022-05-02 15:07:36 -0500

    added listDOM

--- practice/main7.js
+++ practice/main7.js
@@ -50,3 +50,26 @@ let availableItems = [
 let myCart = new ShoppingCart()
 
 //listDOM
+let listDOM = document.getElementById("list")
+for(let i = 0; i < availableItems.length; i++) {
+const item = availableItems[i]
+
+let nameDOM = document.createElement("div")
+nameDOM.style = "grid-column:1 / 2;"
+nameDOM.innerHTML = `${item.name}`
+listDOM.appendChild(nameDOM)
+
+let priceDOM = document.createElement("div")
+priceDOM.style = "grid-column: 2 / 3"
+priceDOM.innerHTML = `${item.price}`
+listDOM.appendChild(priceDOM)
+
+let addToCartDOM = document.createElement("div")
+addToCartDOM.style = "grid-column: 3 / 4;"
+addToCartDOM.className = "material-icons"
+addToCartDOM.innerHTML = "add_shopping_cart"
+addToCartDOM.addEventListener = ("click", ()=>addItemToCart(myCart, item))
+listDOM.appendChild(addToCartDOM)
+
+
+}
