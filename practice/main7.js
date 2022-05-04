4a42f9bf7c863077f7c64d2494c6c04c9d5219c6
EmiKusaba <eeemi615@gmail.com>
2022-05-04 15:23:30 -0500

    added getToalCost

--- practice/main7.js
+++ practice/main7.js
@@ -1,6 +1,6 @@
 //Item
 class Item {
-  constructor(name,price) {
+  constructor(name, price) {
     this.price = price
     this.name = name
   }
@@ -8,33 +8,38 @@ class Item {
 
 //ItemAndQuantity
 class ItemAndQuantity {
-  constructor(item,quantity) {
-   this.item = item
-   this.quantity = quantity
+  constructor(item, quantity) {
+    this.item = item
+    this.quantity = quantity
   }
 }
 //ShoppingCart
 
 class ShoppingCart {
- constructor() {
-   this.itemAndQuantities = []
- }
+  constructor() {
+    this.itemAndQuantities = []
+  }
 
- add(item) {
-    for(let i = 0; i < this.itemAndQuantities.length; i++) {
+  add(item) {
+    for (let i = 0; i < this.itemAndQuantities.length; i++) {
       let existingItemAndQuantity = this.itemAndQuantities[i]
-      if(existingItemAndQuantity.item.name === item.name)
-      existingItemAndQuantity.quantity += 1
+      if (existingItemAndQuantity.item.name === item.name)
+        existingItemAndQuantity.quantity += 1
       return
     }
     this.itemAndQuantities.push(
       new ItemAndQuantity(item, 1)
     )
- }
+  }
 
- getTotalCost() {
-
- }
+  getTotalCost() {
+    let totalCost = 0
+    for (let i = 0; i < this.itemAndQuantities.length; i++) {
+      let itemAndQuantity = this.itemAndQuantities[i]
+      totalCost += itemAndQuantity.item.price * itemAndQuantity.quantity
+    }
+    return totalCost
+  }
 }
 
 
@@ -51,25 +56,25 @@ let myCart = new ShoppingCart()
 
 //listDOM
 let listDOM = document.getElementById("list")
-for(let i = 0; i < availableItems.length; i++) {
-const item = availableItems[i]
+for (let i = 0; i < availableItems.length; i++) {
+  const item = availableItems[i]
 
-let nameDOM = document.createElement("div")
-nameDOM.style = "grid-column:1 / 2;"
-nameDOM.innerHTML = `${item.name}`
-listDOM.appendChild(nameDOM)
+  let nameDOM = document.createElement("div")
+  nameDOM.style = "grid-column:1 / 2;"
+  nameDOM.innerHTML = `${item.name}`
+  listDOM.appendChild(nameDOM)
 
-let priceDOM = document.createElement("div")
-priceDOM.style = "grid-column: 2 / 3"
-priceDOM.innerHTML = `${item.price}`
-listDOM.appendChild(priceDOM)
+  let priceDOM = document.createElement("div")
+  priceDOM.style = "grid-column: 2 / 3"
+  priceDOM.innerHTML = `${item.price}`
+  listDOM.appendChild(priceDOM)
 
-let addToCartDOM = document.createElement("div")
-addToCartDOM.style = "grid-column: 3 / 4;"
-addToCartDOM.className = "material-icons"
-addToCartDOM.innerHTML = "add_shopping_cart"
-addToCartDOM.addEventListener = ("click", ()=>addItemToCart(myCart, item))
-listDOM.appendChild(addToCartDOM)
+  let addToCartDOM = document.createElement("div")
+  addToCartDOM.style = "grid-column: 3 / 4;"
+  addToCartDOM.className = "material-icons"
+  addToCartDOM.innerHTML = "add_shopping_cart"
+  addToCartDOM.addEventListener = ("click", () => addItemToCart(myCart, item))
+  listDOM.appendChild(addToCartDOM)
 
 
 }
