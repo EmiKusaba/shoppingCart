4862ee95090e9de1ad81f205e4a93aac63c3ea75
EmiKusaba <eeemi615@gmail.com>
2022-05-13 14:36:36 -0500

    added class Item and ItemAndQuantity

---
+++ practice/main8.js
@@ -0,0 +1,17 @@
+//Item
+class Item {
+  constructor(name,price) {
+    this.name = name
+    this.price = price
+  }
+}
+
+//ItemAndQuantitiy
+class ItemAndQuantitiy {
+  constructor(item,quantity) {
+    this.item = item
+    this.quantity = quantity
+  }
+}
+
+//ShoppingCart
