73005a087b4611c01ef68433da0380451d50ca20
EmiKusaba <eeemi615@gmail.com>
2022-05-14 15:09:22 -0500

    added available Items

--- practice/main8.js
+++ practice/main8.js
@@ -15,3 +15,18 @@ class ItemAndQuantitiy {
 }
 
 //ShoppingCart
+
+
+//availableItems
+
+
+const availableItems = [
+  new Item("Coffee", 2.00),
+  new Item("Tea", 2.00),
+  new Item("Milk", 1.00),
+  new Item("Chocolate Cookie", 1.00),
+  new Item("Apple Pie", 3.00),
+]
+
+
+//listDOM
